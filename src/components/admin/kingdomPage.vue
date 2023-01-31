2f5de23b616ab63cc2528e579962fbb06e65678d
ArmMie RatChaNoN <armmie3310@gmail.com>
2023-01-31 15:56:34 +0700

    update

--- src/components/admin/kingdomPage.vue
+++ src/components/admin/kingdomPage.vue
@@ -1,111 +1,97 @@
 <template>
-  <v-form>
-    <header class="headerkingdom">สร้างอาณาจักร</header>
+  <v-app>
+    <headerPage title="สร้างอาณาจักร"></headerPage>
     <v-container>
       <v-row>
-        <v-layout align-center justify-center mt-20>
-          <v-col>
-            <!-- <div class="titlekingdom"><v-title>สร้างอาณาจักร</v-title></div> -->
-            <v-card-text>
-              <form>
-                <h2 class="title1">ชื่ออาณาจักร</h2>
-                <v-text-field
-                  class="form1"
-                  placeholder="กรอกชื่ออาณาจักร"
-                  outlined
-                ></v-text-field>
-                <h2 class="title2">รายละเอียด</h2>
-                <v-textarea
-                  class="form2"
-                  placeholder="กรอกรายละเอียด..."
-                  auto-grow
-                  outlined
-                ></v-textarea>
-                <h2 class="titleupload">อัปโหลดภาพหน้าปกอาณาจักร</h2>
-                <!--UPLOAD-->
-                <form enctype="multipart/form-data" novalidate>
-                  <div class="dropbox">
-                    <input
-                      type="file"
-                      multiple
-                      :name="uploadFieldName"
-                      :disabled="isSaving"
-                      accept="image/*"
-                      class="input-file"
-                    />
-                    <p>
-                      Drop file here or click to upload
-                    </p>
-                    <!-- <p v-if="isSaving">Uploading {{ fileCount }} files...</p> -->
-                  </div>
-                </form>
-                <div class="btn text-center">
-                  <v-btn class="btn1" outlined color="error">ยกเลิก</v-btn>
-                  <v-btn class="btn2">ยืนยัน</v-btn>
-                </div>
-              </form>
-            </v-card-text>
-          </v-col>
-        </v-layout>
+        <v-col>
+          <h2 class="title1 mb-2">ชื่ออาณาจักร</h2>
+          <v-text-field
+            class="form1 mb-8"
+            placeholder="กรอกชื่ออาณาจักร"
+            outlined
+            v-model="namekingdom"
+          ></v-text-field>
+          <h2 class="title2 mb-2">รายละเอียด</h2>
+          <v-textarea
+            class="form2"
+            placeholder="กรอกรายละเอียด..."
+            auto-grow
+            outlined
+            v-model="detail"
+          ></v-textarea>
+          <h2 class="titleupload">อัปโหลดภาพหน้าปกอาณาจักร</h2>
+          <!--UPLOAD-->
+          <form enctype="multipart/form-data" novalidate>
+            <div class="dropbox">
+              <input
+                type="file"
+                multiple
+                :name="uploadFieldName"
+                :disabled="isSaving"
+                accept="image/*"
+                class="input-file"
+              />
+              <p>Drop file here or click to upload</p>
+              <!-- <p v-if="isSaving">Uploading {{ fileCount }} files...</p> -->
+            </div>
+          </form>
+          <div class="btn text-center">
+            <v-btn class="btn1" outlined color="error">ยกเลิก</v-btn>
+            <v-btn class="btn2">ยืนยัน</v-btn>
+          </div>
+        </v-col>
       </v-row>
     </v-container>
-  </v-form>
+  </v-app>
 </template>
 
 <script>
-export default {};
+import headerPage from "@/components/header/headerPage.vue";
+export default {
+  name: "kingdomPage",
+  components: {
+    headerPage,
+  },
+  props: {
+    title: { type: String },
+  },
+  data: () => ({}),
+
+  created() {},
+
+  mounted() {},
+
+  watch: {},
+
+  computed: {},
+
+  methods: {},
+};
 </script>
 
 <style scoped>
-.headerkingdom {
-  font-style: normal;
-  font-weight: 700;
-  font-size: 28px;
-  margin-left: 30px;
-  margin-top: 30px;
-  margin-bottom: -70px;
-  padding-left: 60px;
-}
 .title1 {
-  margin-bottom: 10px;
-  margin-top: 0px;
-  padding-left: 60px;
-  font-family: "Roboto";
-  font-style: normal;
   font-weight: 500;
   font-size: 20px;
   line-height: 23px;
 }
 .title2 {
-  margin-bottom: 10px;
-  margin-top: 25px;
-  padding-left: 60px;
-  font-family: "Roboto";
-  font-style: normal;
   font-weight: 500;
   font-size: 20px;
   line-height: 23px;
 }
 .titleupload {
-  margin-bottom: 10px;
-  margin-top: 15px;
-  padding-left: 60px;
-  font-family: "Roboto";
-  font-style: normal;
   font-weight: 500;
   font-size: 20px;
   line-height: 23px;
-  color: #000000;
 }
 .form1 {
-  width: 90%;
+  width: 95%;
   height: 65px;
-  padding-left: 60px;
 }
 .form2 {
-  width: 90%;
+  width: 95%;
   resize: none;
-  padding-left: 60px;
 }
 .btn {
   margin-top: 150px;
@@ -117,42 +103,37 @@ export default {};
   margin-left: 30px;
 }
 .theme--light.v-btn.btn1 {
-  color: red;
+  color: #ff6b81;
 }
 .theme--light.v-btn.btn2 {
   color: white;
 }
 .theme--light.v-btn.v-btn--has-bg {
-  background-color: #f30606;
+  background-color: #ff6b81;
 }
 .dropbox {
   outline: 2px dashed rgb(22, 20, 20); /* the dash box */
   outline-offset: -10px;
   background: white;
   color: rgb(11, 11, 11);
-  /* padding-left: 35px; */
+  margin-left: -8px;
   width: 40%;
-  height: 40%;
+  height: 90px;
   position: relative;
   cursor: pointer;
-  margin-left: 55px;
 }
 
 .input-file {
   opacity: 0; /* invisible but it's there! */
   width: 100%;
-  height: 115px;
+  height: 90px;
   position: absolute;
   cursor: pointer;
 }
-
-.dropbox:hover {
-  background: white; /* when mouse over to the drop zone, change color */
-}
-
 .dropbox p {
-  font-size: 1.7em;
+  font-size: 1.5em;
   text-align: center;
-  padding: 50px 0;
+  padding: 30px 0;
+ 
 }
 </style>
