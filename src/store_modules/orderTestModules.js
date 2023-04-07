2d247b2cd6ed5392f6aca8bca1c0445de37cb829
JIRAYUT <jirayut_no.1@hotmail.com>
2023-04-07 16:07:58 +0700

    course update

--- src/store_modules/orderTestModules.js
+++ src/store_modules/orderTestModules.js
@@ -396,10 +396,7 @@ const orderModules = {
                         'Authorization' : `Bearer ${VueCookie.get("token")}`
                     }
                 }
-<<<<<<< HEAD
-=======
                 // let endpoint = "http://localhost:3002"
->>>>>>> develop
                 let { data } = await axios.get(`${process.env.VUE_APP_URL}/api/v1/order/cart/${account_id}`,config)
                 console.log(data)
                 if (data.statusCode === 200) {
