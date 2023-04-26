abd4380a429f4a2a69918d390b5d4a7ab2ce1e53
Not <email@example.co>
2023-04-26 16:35:25 +0700

    chenge endpoint

--- src/store_modules/courseMonitorModules.js
+++ src/store_modules/courseMonitorModules.js
@@ -13,7 +13,7 @@ const courseMonitorModules = {
     async GetShortCourseMonitor(context,{course_id}){
         try{
             let localhost = "http://localhost:3000"
-            let {data} = await axios.get(`${localhost}/api/v1/monitor/short/courseId/${course_id}`)
+            let {data} = await axios.get(`${process.env.VUE_APP_URL}/api/v1/monitor/short/courseId/${course_id}`)
             if(data.statusCode === 200){
                 context.commit("SetCourseMonitors",data.data)
             }
