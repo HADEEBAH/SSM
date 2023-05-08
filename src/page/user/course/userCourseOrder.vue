fae0f8ac74e45df2fa8b628899d0268aa86396d1
Not <email@example.co>
2023-05-08 11:52:11 +0700

    edit close pre data

--- src/page/user/course/userCourseOrder.vue
+++ src/page/user/course/userCourseOrder.vue
@@ -45,7 +45,7 @@
       </ImgCard>
       <!-- SELECT CLASS DATE -->
       <template v-if="course_order.course_type_id == 'CT_1'">
-        <pre>{{  course_data.days_of_class  }}</pre>
+        <!-- <pre>{{  course_data.days_of_class  }}</pre> -->
         <v-row dense>
           <v-col class="text-lg font-bold"> เลือกช่วงวันเรียน </v-col>
         </v-row>
