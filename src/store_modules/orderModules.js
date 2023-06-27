5f4a11e415e7ff52f5765fa0430e3fa32ec096ce
Jirayut Changruae <jirayut.ch@inet.co.th>
2023-06-27 18:22:07 +0000

    Update orderModules.js

--- src/store_modules/orderModules.js
+++ src/store_modules/orderModules.js
@@ -588,8 +588,9 @@ const orderModules = {
                                 "total": data.data.totalPrice,
                             }
                             console.log(payment_payload)
-                            let localhost = "http://localhost:3003"
-                            let  payment = await axios.patch(`${localhost}/api/v1/payment/data/${data.data.orderNumber}`,payment_payload)
+                            // let localhost = "http://localhost:3003"
+                            let endpoint = process.env.VUE_APP_URL
+                            let  payment = await axios.patch(`${endpoint}/api/v1/payment/data/${data.data.orderNumber}`,payment_payload)
                             if(payment.data.statusCode === 200){
                                 Swal.fire({
                                     icon:"success",
