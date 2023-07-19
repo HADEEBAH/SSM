6ae508217c11b81c963f40a76b2a3c4a67c742b1
Not <email@example.co>
2023-07-19 20:32:14 +0700

    edit-endpoint

--- src/store_modules/manageScheduleModules.js
+++ src/store_modules/manageScheduleModules.js
@@ -427,8 +427,8 @@ const manageScheduleModules = {
         // // console.log("params", params.join("&&"));
 
         // const endpoint = `http://localhost:3000/api/v1/admincourse/filter-schedule?${params.join("&&")}`
-        let localhost = "http://localhost:3000"
-        const endpoint = `${localhost}/api/v1/admincourse/filter-schedule?${params.join("&&")}`
+        // let localhost = "http://localhost:3000"
+        const endpoint = `${process.env.VUE_APP_URL}/api/v1/admincourse/filter-schedule?${params.join("&&")}`
         let { data } = await axios.get(endpoint, config);
         const res = data.data;
 
