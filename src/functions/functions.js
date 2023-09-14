d553f1490a7f1ccdbeef44d29fa8fa4e2f0f8271
Not <email@example.co>
2023-09-14 15:23:40 +0700

    Bug/Bil pdf eng convertToEnglishCurrency

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -201,7 +201,7 @@ export const convertToEnglishCurrency = (number) => {
         }
 
         const lastTwoDigits = num % 100;
-        console.log("lastTwoDigits",lastTwoDigits)
+        // console.log("lastTwoDigits",lastTwoDigits)
         if (lastTwoDigits >= 20) {
             parts.push(tens[Math.floor(lastTwoDigits / 10)]);
             if (lastTwoDigits % 10 > 0) {
