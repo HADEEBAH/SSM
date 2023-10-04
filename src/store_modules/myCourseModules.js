1328054abbefb6a4ab409dab9c98caf2f0973be8
Jirayut Changruae <jirayut.ch@inet.co.th>
2023-10-04 02:44:37 +0000

    Update myCourseModules.js

--- src/store_modules/myCourseModules.js
+++ src/store_modules/myCourseModules.js
@@ -190,7 +190,6 @@ const myCourseModules = {
                     } else {
                         context.commit("SetStudentData", data.data)
                     }
-                    console.log("246 => data", dataCourseSchedule)
                     context.commit("SetcourseSchedule", dataCourseSchedule);
                     context.commit("SetStudentsLoading", false)
                 } else {
@@ -259,7 +258,6 @@ const myCourseModules = {
                         booked.courseImg = booked.courseImg ? `${process.env.VUE_APP_URL}/api/v1/files/${booked.courseImg}` : null
                     }
                     context.commit("SetProfileBooked", data.data)
-                    console.log("SetProfileBooked", data.data)
                 } else {
                     throw { error: data };
                 }
@@ -283,7 +281,6 @@ const myCourseModules = {
                 }
                 // let { data } = await axios.get(`http://localhost:3000/api/v1/mycourse/checkin/student/${account_id}/course/${course_id}`, config);
                 let { data } = await axios.get(`${process.env.VUE_APP_URL}/api/v1/mycourse/checkin/student/${account_id}/course/${course_id}`, config);
-                console.log('data.data :>> ', data.data);
 
                 if (data.data && data.statusCode === 200) {
                     if (data.data.checkIn) {
