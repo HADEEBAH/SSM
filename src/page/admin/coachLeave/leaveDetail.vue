b5a8d5617160ffa146dffca34cd9cd2c13938bbd
Not <email@example.co>
2023-10-18 15:05:00 +0700

    merge develop

--- src/page/admin/coachLeave/leaveDetail.vue
+++ src/page/admin/coachLeave/leaveDetail.vue
@@ -644,17 +644,7 @@ export default {
         .focus();
     },
     InputDate(date, course) {
-<<<<<<< HEAD
       course.compensationDate_str = date
-=======
-      // const options = {
-      //   year: "numeric",
-      //   month: "short",
-      //   day: "numeric"
-      // };
-      // console.log(new Date(date).toLocaleDateString(this.$i18n.locale =='th' ? "th-TH" : "en-US", options))
-      course.compensationDate_str = date;
->>>>>>> develop
     },
     closeDisapprovedDialog() {
       this.show_disapproved = false;
