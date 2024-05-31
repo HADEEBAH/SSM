7a81c5a3a6dae441fb095e52bb9e9b39d34f22c5
HADIBAH MA <hadeebah64@gmail.com>
2024-06-01 02:18:29 +0700

    feature table report

--- src/page/admin/satisfaction/satisfactionReport.vue
+++ src/page/admin/satisfaction/satisfactionReport.vue
@@ -1,157 +1,305 @@
 <template>
-    <v-container>
-        <headerPage :title="$t('satisfaction score')"></headerPage>
-        <v-row class="mb-3">
-            <v-col cols="12" lg="3">
-                <v-card outlined rounded="lg">
-                    <v-card-text class="text-left">
-                        <v-row>
-                            <v-col class="d-flex justify-center align-center">
-                                <v-icon size="50" color="#89d767">mdi-chart-areaspline</v-icon>
-                            </v-col>
-                            <v-col align="center">
-                                <div class="text-base font-bold">{{ $t('average') }}</div>
-                                <span class="text-xl font-bold">{{ satisfactions.AverageRate }}</span>
-                            </v-col>
-                        </v-row>
-                    </v-card-text>
-                </v-card>
-            </v-col>
-            <v-col cols="12" lg="">
+  <v-container>
+    <headerPage :title="$t('satisfaction score')"></headerPage>
+    <v-row class="mb-3">
+      <v-col cols="12" lg="3">
+        <v-card outlined rounded="lg">
+          <v-card-text class="text-left">
+            <v-row>
+              <v-col class="d-flex justify-center align-center">
+                <v-icon size="50" color="#89d767">mdi-chart-areaspline</v-icon>
+              </v-col>
+              <v-col align="center">
+                <div class="text-base font-bold">{{ $t("average") }}</div>
+                <span class="text-xl font-bold">{{
+                  satisfactions.AverageRate
+                }}</span>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" lg="">
+        <v-row>
+          <v-col cols="6" lg="">
+            <v-card outlined rounded="lg">
+              <v-card-text>
                 <v-row>
-                    <v-col cols="6" lg="">
-                        <v-card outlined rounded="lg">
-                            <v-card-text>
-                                <v-row>
-                                    <v-col align="center">
-                                        <v-img height="50" width="50" src="@/assets/star/5-stars.png"></v-img>
-                                    </v-col>
-                                    <v-col class="d-flex align-center justify-center">
-                                        <span class="text-lg font-bold">{{ satisfactions.fiveStar }}</span>
-                                    </v-col>
-                                </v-row>
-                            </v-card-text>
-                        </v-card>
-                    </v-col>
-                    <v-col cols="6" lg="">
-                        <v-card outlined rounded="lg">
-                            <v-card-text>
-                                <v-row>
-                                    <v-col align="center">
-                                        <v-img height="50" width="50" src="@/assets/star/4-stars.png"></v-img>
-                                    </v-col>
-                                    <v-col class="d-flex align-center justify-center">
-                                        <span class="text-lg font-bold">{{ satisfactions.fourStar }}</span>
-                                    </v-col>
-                                </v-row>
-                            </v-card-text>
-                        </v-card>
-                    </v-col>
-                    <v-col cols="6" lg="">
-                        <v-card outlined rounded="lg">
-                            <v-card-text>
-                                <v-row>
-                                    <v-col align="center">
-                                        <v-img height="50" width="50" src="@/assets/star/3-stars.png"></v-img>
-                                    </v-col>
-                                    <v-col class="d-flex align-center justify-center">
-                                        <span class="text-lg font-bold">{{ satisfactions.threeStar }}</span>
-                                    </v-col>
-                                </v-row>
-                            </v-card-text>
-                        </v-card>
-                    </v-col>
-                    <v-col cols="6" lg="">
-                        <v-card outlined rounded="lg">
-                            <v-card-text>
-                                <v-row>
-                                    <v-col align="center">
-                                        <v-img height="25" width="25" src="@/assets/star/star.png"></v-img>
-                                        <v-img height="25" width="25" src="@/assets/star/star.png"></v-img>
-                                    </v-col>
-                                    <v-col class="d-flex align-center justify-center">
-                                        <span class="text-lg font-bold">{{ satisfactions.twoStar }}</span>
-                                    </v-col>
-                                </v-row>
-                            </v-card-text>
-                        </v-card>
-                    </v-col>
-                    <v-col cols="6" lg="">
-                        <v-card outlined rounded="lg">
-                            <v-card-text>
-                                <v-row>
-                                    <v-col align="center">
-                                        <v-img height="50" width="50" src="@/assets/star/star.png"></v-img>
-                                    </v-col>
-                                    <v-col class="d-flex align-center justify-center">
-                                        <span class="text-lg font-bold">{{ satisfactions.oneStar }}</span>
-                                    </v-col>
-                                </v-row>
-                            </v-card-text>
-                        </v-card>
-                    </v-col>
+                  <v-col align="center">
+                    <v-img
+                      height="50"
+                      width="50"
+                      src="@/assets/star/5-stars.png"
+                    ></v-img>
+                  </v-col>
+                  <v-col class="d-flex align-center justify-center">
+                    <span class="text-lg font-bold">{{
+                      satisfactions.fiveStar
+                    }}</span>
+                  </v-col>
                 </v-row>
-            </v-col>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col cols="6" lg="">
+            <v-card outlined rounded="lg">
+              <v-card-text>
+                <v-row>
+                  <v-col align="center">
+                    <v-img
+                      height="50"
+                      width="50"
+                      src="@/assets/star/4-stars.png"
+                    ></v-img>
+                  </v-col>
+                  <v-col class="d-flex align-center justify-center">
+                    <span class="text-lg font-bold">{{
+                      satisfactions.fourStar
+                    }}</span>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col cols="6" lg="">
+            <v-card outlined rounded="lg">
+              <v-card-text>
+                <v-row>
+                  <v-col align="center">
+                    <v-img
+                      height="50"
+                      width="50"
+                      src="@/assets/star/3-stars.png"
+                    ></v-img>
+                  </v-col>
+                  <v-col class="d-flex align-center justify-center">
+                    <span class="text-lg font-bold">{{
+                      satisfactions.threeStar
+                    }}</span>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col cols="6" lg="">
+            <v-card outlined rounded="lg">
+              <v-card-text>
+                <v-row>
+                  <v-col align="center">
+                    <v-img
+                      height="25"
+                      width="25"
+                      src="@/assets/star/star.png"
+                    ></v-img>
+                    <v-img
+                      height="25"
+                      width="25"
+                      src="@/assets/star/star.png"
+                    ></v-img>
+                  </v-col>
+                  <v-col class="d-flex align-center justify-center">
+                    <span class="text-lg font-bold">{{
+                      satisfactions.twoStar
+                    }}</span>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col cols="6" lg="">
+            <v-card outlined rounded="lg">
+              <v-card-text>
+                <v-row>
+                  <v-col align="center">
+                    <v-img
+                      height="50"
+                      width="50"
+                      src="@/assets/star/star.png"
+                    ></v-img>
+                  </v-col>
+                  <v-col class="d-flex align-center justify-center">
+                    <span class="text-lg font-bold">{{
+                      satisfactions.oneStar
+                    }}</span>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+            </v-card>
+          </v-col>
         </v-row>
-        <div>
-            <v-data-table
-                class="elevation-1 header-table"
-                :headers="column"
-                :items="satisfactions.list"
-            ></v-data-table>
-        </div>
-    </v-container>
+      </v-col>
+    </v-row>
+    <!-- <pre>{{ getLoop }}</pre> -->
+    <div>
+      <v-simple-table style="width: 100%" height="289px" class="my-5 table">
+        <template v-slot:default>
+          <thead fixed-header>
+            <tr class="text-center">
+              <th class="text-center" colspan="8">ความพึงพอใจของระบบ</th>
+
+              <th colspan="4" class="text-center">ความพึงพอใจของ Dashboard</th>
+            </tr>
+          </thead>
+          <thead>
+            <tr>
+              <th class="text-center" colspan="1">ภาพรวมการใช้บริการ</th>
+              <th class="text-center" colspan="1">ความง่ายและดวกในการใช้งาน</th>
+              <th class="text-center" colspan="1">ความเสถียรภาพของบริการ</th>
+              <th class="text-center" colspan="5">
+                คำแนะนำ ความพึงพอใจของระบบ
+              </th>
+              <th class="text-center" colspan="1">
+                การแสดงผลและการออกแบบระบบของ Dashboard
+              </th>
+              <th class="text-center" colspan="3">
+                คำแนะนำ ความพึงพอใจของ Dashboard
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="user in transformedData" :key="user.accountId">
+              <td class="text-center" colspan="1">
+                {{ user["ภาพรวมการใช้บริการ"] || "0" }}
+              </td>
+              <td class="text-center" colspan="1">
+                {{ user["ความง่ายและดวกในการใช้งาน"] || "0" }}
+              </td>
+              <td class="text-center" colspan="1">
+                {{ user["ความเสถียรภาพของบริการ"] || "0" }}
+              </td>
+              <td class="text-center" colspan="5">
+                {{ user["ความพึงพอใจของระบบ Remark"] || "" }}
+              </td>
+              <td class="text-center" colspan="1">
+                {{
+                  user[
+                    "การแสดงผลและการออกแบบระบบ เข้าใจง่าย มีข้อมูลครบตามต้องการ"
+                  ] || "0"
+                }}
+              </td>
+              <td class="text-center" colspan="3">
+                {{ user["ความพึงพอใจของ Dashboard Remark"] || "" }}
+              </td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+      <!-- <v-data-table
+        class="elevation-1 header-table"
+        :headers="column"
+        :items="satisfactions.list"
+      ></v-data-table> -->
+    </div>
+  </v-container>
 </template>
 
 <script>
-import headerPage from "@/components/header/headerPage.vue"
+import headerPage from "@/components/header/headerPage.vue";
 import { mapActions, mapGetters } from "vuex";
 // const star = require("@/assets/star/star.png")
 export default {
-    name: "SatifactionReport",
-    components:{ headerPage },
-    computed:{
-        ...mapGetters({
-            satisfactions : "satisfactionModules/satisfactions"
-        }),
-        column() {
-            return [
-                {
-                    text: this.$t("first name - last name"),
-                    align: "center",
-                    sortable: false,
-                    value: this.$i18n.locale == "th" ? "nameTh" : "nameEn",
-                },
-                {
-                    text: this.$t("rate"),
-                    align: "center",
-                    sortable: true,
-                    value: "rate",
-                },
-                {
-                    text: this.$t("suggestions"),
-                    align: "start",
-                    sortable: false,
-                    value: "remark",
-                },
-            ];
+  name: "SatifactionReport",
+  data() {
+    return {
+      transformedData: [],
+      questionsSet: [],
+      surveysSet: [],
+    };
+  },
+  components: { headerPage },
+  computed: {
+    ...mapGetters({
+      satisfactions: "satisfactionModules/satisfactions",
+    }),
+    column() {
+      return [
+        {
+          text: this.$t("first name - last name"),
+          align: "center",
+          sortable: false,
+          value: this.$i18n.locale == "th" ? "nameTh" : "nameEn",
         },
+        {
+          text: this.$t("rate"),
+          align: "center",
+          sortable: true,
+          value: "nameEn",
+        },
+        {
+          text: this.$t("suggestions"),
+          align: "start",
+          sortable: false,
+          value: "remark",
+        },
+      ];
     },
-    created(){
-        const user_detail = JSON.parse(localStorage.getItem("userDetail"));
-        if(!['yuthyuth', 'jirayut2539'].includes(user_detail.username)){
-            this.$router.push({ name: "Dashboard" });
-        }
-        this.GetSatisfactionList()
-    },
-    methods: {
-        ...mapActions({
-            GetSatisfactionList : "satisfactionModules/GetSatisfactionList"
-        })
+  },
+  async created() {
+    const user_detail = JSON.parse(localStorage.getItem("userDetail"));
+    if (!["yuthyuth", "jirayut2539"].includes(user_detail.username)) {
+      await this.$router.push({ name: "Dashboard" });
     }
-}
+    await this.GetSatisfactionList();
+    await this.transformData();
+  },
+  async mounted() {
+    await this.GetSatisfactionList();
+    await this.transformData();
+  },
+  methods: {
+    ...mapActions({
+      GetSatisfactionList: "satisfactionModules/GetSatisfactionList",
+    }),
+    transformData() {
+      const surveyList = this.satisfactions.list;
+      const normalizedData = [];
+      const questionsSet = new Set();
+      const surveysSet = new Set();
+
+      surveyList.forEach((user) => {
+        const userData = {
+          accountId: user.accountId,
+          nameTh: user.nameTh,
+          nameEn: user.nameEn,
+          createdDate: user.createdDate,
+        };
+
+        user.surveys.forEach((survey) => {
+          if (
+            survey.title === "ความพึงพอใจของระบบ" ||
+            survey.title === "ความพึงพอใจของ Dashboard"
+          ) {
+            survey.questions.forEach((question) => {
+              const key = `${question.questionText}`;
+              userData[key] = question.rate;
+              questionsSet.add(key);
+            });
+
+            // Add survey remark
+            const remarkKey = `${survey.title}`;
+            userData[remarkKey] = survey.remark || "";
+            surveysSet.add(survey.title);
+          }
+        });
+
+        normalizedData.push(userData);
+      });
+
+      // Sort questionsSet and surveysSet alphabetically
+      this.questionsSet = Array.from(questionsSet);
+      this.surveysSet = Array.from(surveysSet);
+
+      this.transformedData = normalizedData;
+    },
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+.table,
+th,
+td {
+  border: 1px solid black;
+  /* border-collapse: collapse; */
+}
 </style>
