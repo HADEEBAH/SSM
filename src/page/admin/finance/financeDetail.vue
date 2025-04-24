c967db09511916a6d9ff575300bb44ae043b7f00
HADIBAH MA <hadeebah64@gmail.com>
2025-04-24 09:21:27 +0700

    edit defect 1372

--- src/page/admin/finance/financeDetail.vue
+++ src/page/admin/finance/financeDetail.vue
@@ -1176,8 +1176,6 @@ export default {
             stack: [
               {
                 text: `${
-                  this.pdf_lang == "th" ? "อาณาจักร" : "Category Name"
-                }: ${
                   this.pdf_lang == "th"
                     ? course.course.categoryNameTh
                     : course.course.categoryNameEng
@@ -1186,8 +1184,6 @@ export default {
               },
               {
                 text: `${
-                  this.pdf_lang == "th" ? "คอร์สเรียน" : "Course Name"
-                }: ${
                   this.pdf_lang == "th"
                     ? course.course.courseNameTh
                     : course.course.courseNameEn
@@ -1197,13 +1193,13 @@ export default {
                 color: "#ff6b81",
               },
               {
-                text: `${this.pdf_lang == "th" ? "แพ็กเกจ" : "Package Name"}: ${
+                text: `${
                   course?.cpo?.packageName ? course.cpo.packageName : ""
                 }`,
                 color: "#ff6b81",
               },
               {
-                text: `${this.pdf_lang == "th" ? "ช่วงเวลา" : "Option"}: ${
+                text: `${
                   course?.cpo?.optionName
                     ? this.pdf_lang == "th"
                       ? course.cpo.optionName
